f9a27d8cfb6d7819d91e16b5546f7827dd4c772d
tshort <tshort.rlists@gmail.com>
2014-02-10 17:57:42 -0500

    Add support for macrocalls and for begin ... end blocks.

--- src/Lazy.jl
+++ src/Lazy.jl
@@ -16,23 +16,26 @@ end
 
 # Threading macros
 
-macro >(x, expr, exprs...)
-  if typeof(expr) == Expr && expr.head == :tuple
-    return Expr(:macrocall, symbol("@>"), esc(x), map(esc,expr.args)..., map(esc,exprs)...)
+thread_left(x) = thread_left(filter(x-> !isa(x,Expr) || x.head != :line, x.args)...)
 
-  elseif typeof(expr) == Symbol
-    call = esc(Expr(:call, expr, x))
+function thread_left(x, expr, exprs...)
+  if typeof(expr) == Symbol
+    call = Expr(:call, expr, x)
 
-  elseif typeof(expr) == Expr && expr.head == :call
-    call = esc(Expr(:call, expr.args[1], x, expr.args[2:]...))
+  elseif typeof(expr) == Expr && expr.head in [:call, :macrocall]
+    call = Expr(expr.head, expr.args[1], x, expr.args[2:]...)
 
   elseif typeof(expr) == Expr && expr.head == :->
-    call = esc(Expr(:call, expr, x))
+    call = Expr(:call, expr, x)
 
   else
     error("Unsupported expression $expr in @>")
   end
-  isempty(exprs) ? call : :(@> $call $(map(esc,exprs)...))
+  isempty(exprs) ? call : :(@> $call $(exprs...))
+end
+
+macro >(exprs...)
+    esc(thread_left(exprs...))
 end
 
 macro >>(x, expr, exprs...)
