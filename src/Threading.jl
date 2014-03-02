111d656e87f40a2c4e19cea313b0146621a72d87
Mike Innes <mike.j.innes@gmail.com>
2014-03-02 12:48:24 +0000

    cleanup

--- src/Threading.jl
+++ src/Threading.jl
@@ -2,8 +2,8 @@
 
 export @>, @>>, @as
 
-isexpr{T}(x::T, ts...) = T in ts
 isexpr(x::Expr, ts...) = x.head in ts
+isexpr{T}(x::T, ts...) = T in ts
 
 subexprs(ex) = filter(x -> !isexpr(x, :line), ex.args)
 
@@ -11,15 +11,10 @@ macro > (exs...)
   thread(x) = isexpr(x, :block) ? thread(subexprs(x)...) : x
 
   thread(x, ex) =
-    if isexpr(ex, Symbol, :->)
-      Expr(:call, ex, x)
-    elseif isexpr(ex, :call, :macrocall)
-      Expr(ex.head, ex.args[1], x, ex.args[2:end]...)
-    elseif isexpr(ex, :block)
-      thread(x, subexprs(ex)...)
-    else
-      error("Unsupported expression $ex in @>")
-    end
+    isexpr(ex, Symbol, :->)       ? Expr(:call, ex, x) :
+    isexpr(ex, :call, :macrocall) ? Expr(ex.head, ex.args[1], x, ex.args[2:end]...) :
+    isexpr(ex, :block)            ? thread(x, subexprs(ex)...) :
+                                    error("Unsupported expression $ex in @>")
 
   thread(x, exs...) = reduce(thread, x, exs)
 
@@ -30,15 +25,10 @@ macro >> (exs...)
   thread(x) = isexpr(x, :block) ? thread(subexprs(x)...) : x
 
   thread(x, ex) =
-    if isexpr(ex, Symbol, :->)
-      Expr(:call, ex, x)
-    elseif isexpr(ex, :call, :macrocall)
-      Expr(ex.head, ex.args..., x)
-    elseif isexpr(ex, :block)
-      thread(x, subexprs(ex)...)
-    else
-      error("Unsupported expression $ex in @>>")
-    end
+    isexpr(ex, Symbol, :->)       ? Expr(:call, ex, x) :
+    isexpr(ex, :call, :macrocall) ? Expr(ex.head, ex.args..., x) :
+    isexpr(ex, :block)            ? thread(x, subexprs(ex)...) :
+                                    error("Unsupported expression $ex in @>>")
 
   thread(x, exs...) = reduce(thread, x, exs)
 
@@ -49,15 +39,11 @@ macro as (exs...)
   thread(as, x) = isexpr(x, :block) ? thread(as, subexprs(x)...) : x
 
   thread(as, x, ex) =
-    if isexpr(ex, Symbol, :->)
-      Expr(:call, ex, x)
-    elseif isexpr(x, :block)
-      thread(as, x, subexprs(ex))
-    else
-      :(let $as = $x
-          $ex
-        end)
-    end
+    isexpr(ex, Symbol, :->) ? Expr(:call, ex, x) :
+    isexpr(x, :block)       ? thread(as, x, subexprs(ex)) :
+    :(let $as = $x
+        $ex
+      end)
 
   thread(as, x, exs...) = reduce((x, ex) -> thread(as, x, ex), x, exs)
 
