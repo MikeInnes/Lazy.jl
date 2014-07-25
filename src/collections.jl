72b7ebb2a18f150d29719dd6d7ae6cb604da43c3
Mike Innes <mike.j.innes@gmail.com>
2014-07-25 23:43:31 +0100

    implement split for arrays

--- src/collections.jl
+++ src/collections.jl
@@ -1,3 +1,17 @@
 # TODO: Implement liblazy functions over standard arrays / other collections
 
 walk(inner, outer, xs::Array) = @>> xs map(inner) outer
+
+function Base.split{T}(xs::Vector{T}, x; keepempty = false)
+  result = Vector{T}[]
+  push!(result, T[])
+  for i = 1:length(xs)
+    if xs[i] == x
+      (keepempty || !(isempty(result[end]) || i == length(xs))) &&
+        push!(result, T[])
+    else
+      push!(result[end], xs[i])
+    end
+  end
+  return result
+end
