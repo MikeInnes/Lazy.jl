d89e15ce4bcc9c8fdcf0015041791d35ced72782
Mike Innes <mike.j.innes@gmail.com>
2015-12-31 11:07:02 +0000

    make sure method forwarding is inlined

--- src/macros.jl
+++ src/macros.jl
@@ -274,5 +274,7 @@ macro forward(ex, fs)
   @capture(ex, T_.field_) || error("Syntax: @forward T.x f, g, h")
   T = esc(T)
   fs = isexpr(fs, :tuple) ? map(esc, fs.args) : [esc(fs)]
-  :($([:($f(x::$T, args...) = $f(x.$field, args...)) for f in fs]...);nothing)
+  :($([:($f(x::$T, args...) = (Base.@_inline_meta; $f(x.$field, args...)))
+       for f in fs]...);
+    nothing)
 end
