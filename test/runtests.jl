27b424b3e5e7d164abcecfac6a5cf76be420c563
Mike J Innes <mike.j.innes@gmail.com>
2017-10-03 19:49:14 +0100

    get rid of this factcheck bullshit

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,34 +1,36 @@
 using Lazy
 import Lazy: cycle, range, drop, take
-using FactCheck
+using Base.Test
 
-facts("Lists") do
-    @fact list(1, 2, 3)[2] --> 2
-    @fact prepend(1, list(2,3,4)) --> 1:list(2, 3, 4)
-    @fact seq([1, 2, 3]) --> list(1, 2, 3)
-    @pending seq(1:3) --> list(1, 2, 3)
-    @fact constantly(1)[50] --> 1
+@testset "Lazy" begin
+
+@testset "Lists" begin
+    @test list(1, 2, 3)[2] == 2
+    @test prepend(1, list(2,3,4)) == 1:list(2, 3, 4)
+    @test seq([1, 2, 3]) == list(1, 2, 3)
+    @test seq(1:3) == list(1, 2, 3)
+    @test constantly(1)[50] == 1
     testfn() = 1
-    @fact repeatedly(testfn)[50] --> 1
-    @fact cycle([1, 2, 3])[50] --> 2
-    @fact iterate(x->x^2, 2)[3] --> 16
-    @fact range(1, 5)[3] --> 3
-    @fact range(1, 5)[10] --> nothing
-    @fact range(1, 5)[-1] --> 1
-    @fact list(1, 2, 3) * list(4, 5, 6) --> list(1, 2, 3, 4, 5, 6)
-    @fact first(list(1, 2, 3)) --> 1
-    @fact tail(list(1, 2, 3)) --> list(2, 3)
-    @fact flatten(list(1,2,list(3,4))) --> list(1, 2, 3, 4)
-    @fact list(1,2,list(3,4))[3] --> list(3, 4)
+    @test repeatedly(testfn)[50] == 1
+    @test cycle([1, 2, 3])[50] == 2
+    @test iterate(x->x^2, 2)[3] == 16
+    @test range(1, 5)[3] == 3
+    @test range(1, 5)[10] == nothing
+    @test range(1, 5)[-1] == 1
+    @test list(1, 2, 3) * list(4, 5, 6) == list(1, 2, 3, 4, 5, 6)
+    @test first(list(1, 2, 3)) == 1
+    @test tail(list(1, 2, 3)) == list(2, 3)
+    @test flatten(list(1,2,list(3,4))) == list(1, 2, 3, 4)
+    @test list(1,2,list(3,4))[3] == list(3, 4)
 end
 
-facts("Fibs") do
+@testset "Fibs" begin
     fibs = @lazy 0:1:(fibs + drop(1, fibs));
-    @fact fibs[20] --> 4181
-    @fact take(5, fibs) --> list(0, 1, 1, 2, 3)
+    @test fibs[20] == 4181
+    @test take(5, fibs) == list(0, 1, 1, 2, 3)
 end
 
-facts("Primes") do
+@testset "Primes" begin
     isprime(n) =
         @>> primes begin
             take_while(x -> x<=sqrt(n))
@@ -38,41 +40,41 @@ facts("Primes") do
     primes = filter(isprime, range(2));
 end
 
-facts("Even squares") do
+@testset "Even squares" begin
     esquares = @>> range() map(x->x^2) filter(iseven);
-    @fact take(5, esquares) --> list(4, 16, 36, 64, 100)
+    @test take(5, esquares) == list(4, 16, 36, 64, 100)
 end
 
-facts("Threading macros") do
+@testset "Threading macros" begin
     temp = @> [2 3] sum
-    @fact temp --> 5
+    @test temp == 5
     # Reverse from after index 2
     temp = @>> 2 reverse([1, 2, 3, 4, 5])
-    @fact temp --> [1, 5, 4, 3, 2]
+    @test temp == [1, 5, 4, 3, 2]
     temp = @as x 2 begin
         x^2
         x + 2
     end
-    @fact temp --> 6
+    @test temp == 6
 end
 
-facts("Listables") do
-    @fact_throws MethodError sin()
+@testset "Listables" begin
+    @test_throws MethodError sin()
 end
 
-facts("any/all") do
+@testset "any/all" begin
     let xs = list(true, false, false)
-        @pending any(identity, xs) --> true
-        @pending any(xs) --> true
-        @pending all(identity, xs) --> false
-        @pending all(xs) --> false
+        @test any(identity, xs) == true
+        @test any(xs) == true
+        @test all(identity, xs) == false
+        @test all(xs) == false
     end
     let yy = list(1, 0, 1)
-        @pending any(Bool, yy) --> true
-        @pending all(Bool, yy) --> false
+        @test any(Bool, yy) == true
+        @test all(Bool, yy) == false
     end
     # Base method--ensures no ambiguity with methods here
-    @fact all([true true; true true], 1) --> [true true]
+    @test all([true true; true true], 1) == [true true]
 end
 
-FactCheck.exitstatus()
+end
