cb5a1e6b1f5671fa8ab2272c09650a8a96858596
Tom Kwong <tk3369@gmail.com>
2019-11-23 12:12:19 -0800

    Support keyword arguments in forward macro

--- src/macros.jl
+++ src/macros.jl
@@ -282,7 +282,7 @@ macro forward(ex, fs)
   @capture(ex, T_.field_) || error("Syntax: @forward T.x f, g, h")
   T = esc(T)
   fs = isexpr(fs, :tuple) ? map(esc, fs.args) : [esc(fs)]
-  :($([:($f(x::$T, args...) = (Base.@_inline_meta; $f(x.$field, args...)))
+  :($([:($f(x::$T, args...; kwargs...) = (Base.@_inline_meta; $f(x.$field, args...; kwargs...)))
        for f in fs]...);
     nothing)
 end
