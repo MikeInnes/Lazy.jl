db291e9d58717aa7a41a83b41e54f17df0d07993
Tom Kwong <tk3369@gmail.com>
2019-11-29 12:59:07 -0800

    added tests for @forward

--- test/runtests.jl
+++ test/runtests.jl
@@ -101,6 +101,22 @@ end
 
 end
 
+@testset "Forward macro" begin
+    struct Foo112 end
+    play(x::Foo112; y) = y                        # uses keyword arg
+    play(x::Foo112, z) = z                        # uses regular arg
+    play(x::Foo112, z1, z2; y) = y + z1 + z2      # uses both
+
+    struct Bar112 f::Foo112 end                   # composition
+    @forward Bar112.f play                        # forward `play` function to field `f`
+
+    let f = Foo112(), b = Bar112(f)
+        @test play(f, y = 1) === play(b, y = 1) 
+        @test play(f, 2) === play(b, 2) 
+        @test play(f, 2, 3, y = 1) === play(b, 2, 3, y = 1) 
+    end
+end
+
 @testset "Listables" begin
     @test_throws MethodError sin()
 end
